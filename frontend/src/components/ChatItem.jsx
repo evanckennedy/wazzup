f9c66a958daec04e9c7d54f7335292a70b9e966e
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-01 17:28:48 -0500

    pass fake data to chat item

--- frontend/src/components/ChatItem.jsx
+++ frontend/src/components/ChatItem.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import { FaCheck } from 'react-icons/fa';
 
-function ChatItem() {
+function ChatItem({chat}) {
   return (
     <>
     <div className='flex justify-between'>
       <figure>
         <img 
           className='chat-item-pfp'
-          src="https://via.placeholder.com/128" 
-          alt="" 
+          src={chat.avatar || 'https://via.placeholder.com/128'}
+          alt="avatar" 
         />
       </figure>
-      
       <div className='flex flex-column'>
         <div className='flex justify-between'>
-          <div>Cameron</div>
-          <p>7:54 PM</p>
+          <div>{chat.name}</div>
+          <p>{chat.lastMessageTime || '7:54 PM'}</p>
         </div>
         <div className='flex gap-5'>
           <FaCheck />
-          <p>Evan: Okay, sounds good</p>
+          <p>
+            <span>{chat.lastMessageSender || 'You'}</span>
+            :
+            <span>{chat.lastMessage}</span>
+          </p>
         </div>
       </div>
     </div>
--- frontend/src/components/ChatList.jsx
+++ frontend/src/components/ChatList.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ChatItem from './ChatItem'
 
-function ChatList() {
+function ChatList({chats}) {
   return (
     <div>
-      <ChatItem />
+      {chats.map((chat) => (
+        <ChatItem key={chat.id} chat={chat} />
+      ))}
     </div>
     /* overflow-y: auto for this section */
   )
