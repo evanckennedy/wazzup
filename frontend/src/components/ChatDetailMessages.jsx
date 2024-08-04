76b9d8d17bbd9d1c0bec431cf16fe1a978248d7f
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-04 17:50:55 -0500

    add todo comments to code

--- frontend/src/components/ChatDetailMessages.jsx
+++ frontend/src/components/ChatDetailMessages.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 function ChatDetailMessages() {
+  /* 
+    To Do: 
+      - When the user clicks on a chat from the sidebar, the messages should display.
+        Map out each chat.
+      - This component should take up the vast majority (roughly 80 percent) of the 
+        vertical spacing of the chat window.
+      - Add "overflow-y: auto;" to the parent div of this component, allowing
+        users to scroll through the message history
+  */ 
   return (
     <div>ChatDetailMessages
-      {/* <div>
-        //add the messages mapped out. two classes, if the message sender is me, add sent class, otherwise recieved class
-              //inside the div have a chatmessagetime span 
-      </div> */}
+      
     </div>
   )
 }
--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -3,11 +3,15 @@ import DefaultChatView from './DefaultChatView'
 import ChatDetail from './ChatDetail'
 
 function ChatWindow() {
+     /* 
+     To do: 
+      - render ChatDetail component if the user selects a chat, and pass the 
+        selected chat's details as a prop down to the ChatDetailMessages component
+        and the ChatDetailHeader component to display the correct name and avatar 
+      */
   return (
     <div className='chat-window'>
       <DefaultChatView />
-      {/* render ChatDetail component if the user selects a chat, and pass the 
-      selected chat's details as a prop down to the ChatDetailMessages component*/}
     </div>
   )
 }
