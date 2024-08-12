d4a2cf7811025a423ed20c54942e84b379cc9ccf
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-12 18:42:09 -0500

    add validation for registration form

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -6,9 +6,32 @@ function RegisterForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
+  const [errors, setErrors] = useState({});
+
+  // validation function
+  const validate = () => {
+    const newErrors = {};
+    if (!name) newErrors.name = 'Name is required';
+
+    if (!email) newErrors.email = 'Email is required';
+    else if (!/\S+@\S+\.\S+/.test(email)) newErrors.email = 'Email is invalid';
+
+    if (!password) newErrors.password = 'Password is required';
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (validate()) {
+      // submit form
+      console.log('Form submitted')
+    }
+  }
 
   return (
-    <form action="" className='flex flex-column gap-15 auth-form'>
+    <form action="" className='flex flex-column auth-form' onSubmit={handleSubmit}>
       <div className='flex flex-column'>
         <label htmlFor="name" className='auth-label'>Name</label>
         <input 
@@ -19,6 +42,9 @@ function RegisterForm() {
           value={name} 
           onChange={e => setName(e.target.value)}
         />
+        <div className="error-message-container">
+          {errors.name && <span className='error-message'>{errors.name}</span>}
+        </div>
       </div>
       <div className='flex flex-column'>
         <label htmlFor="email" className='auth-label'>Email</label>
@@ -30,6 +56,9 @@ function RegisterForm() {
           value={email}
           onChange={e => setEmail(e.target.value)}
         />
+        <div className="error-message-container">
+          {errors.email && <span className='error-message'>{errors.email}</span>}
+        </div>
       </div>
       <div className='flex flex-column'>
         <label htmlFor="password" className='auth-label'>Password</label>
@@ -41,6 +70,9 @@ function RegisterForm() {
           value={password}
           onChange={e => setPassword(e.target.value)}
         />
+        <div className="error-message-container">
+          {errors.password && <span className='error-message'>{errors.password}</span>}
+        </div>
       </div>
       <AuthButton text='Sign Up' />
     </form>
