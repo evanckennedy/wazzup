d6171c88be9b25a60f2162261611e019e72bbd1e
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-17 22:59:31 -0500

    update user.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,22 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  name: String,
-  password: String,
-  contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Contact' }],
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  contacts: [{ 
+    type: mongoose.Schema.Types.ObjectId, ref: 'Contact' 
+  }],
   profilePicture: String,
   createdAt: {
     type: Date,
