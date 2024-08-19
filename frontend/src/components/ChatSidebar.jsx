540ec2c560b84e6c6e064975ba854007a0d7a783
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-19 14:12:05 -0500

    remove chat search bar

--- frontend/src/components/ChatSidebar.jsx
+++ frontend/src/components/ChatSidebar.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ChatList from './ChatList'
-import ChatSearchBar from './ChatSearchBar'
 import ChatSidebarHeader from './ChatSidebarHeader'
 
 function ChatSidebar({chats}) {
   return (
     <div className='chat-sidebar'>
       <ChatSidebarHeader />
-      <ChatSearchBar />
       <ChatList chats={chats}/>
     </div>
   )
--- frontend/src/components/ChatSidebarHeader.jsx
+++ frontend/src/components/ChatSidebarHeader.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { FaPlus, FaFilter } from 'react-icons/fa';
 
 function ChatSidebarHeader() {
   return (
     <div className='sidebar-header-container flex justify-between'>
       <h3>Chats</h3>
-      <div className='flex gap-5'>
-        <FaPlus />
-        <FaFilter />
-      </div>
+      <h3>Contacts</h3>
     </div>
   )
 }
