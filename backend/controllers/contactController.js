1fd716d938406b298dfe6e4028750abd8d079314
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-21 16:05:33 -0500

    fix authorization typo

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -3,7 +3,7 @@ import { createContact } from "../services/contactService.js";
 // async function to Add contact
 export async function handleCreateContact (req, res) {
   try {
-    const token = req.headers.authoriztion.split(' ')[1] // if the authorization header value is "Bearer abc123xyz", the code will split it into an array ["Bearer", "abc123xyz"], and then assign the token "abc123xyz" to the token variable.
+    const token = req.headers.authorization.split(' ')[1] // if the authorization header value is "Bearer abc123xyz", the code will split it into an array ["Bearer", "abc123xyz"], and then assign the token "abc123xyz" to the token variable.
     const { contactId, nickname } = req.body;
     const newContact = await createContact(token, contactId, nickname)
 
