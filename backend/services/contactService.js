23f372b4895f83af0490e2f1e5265dbe21c365f5
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-21 16:12:20 -0500

    resolve reference error and allow code to access the jwt secret varaible

--- backend/services/contactService.js
+++ backend/services/contactService.js
@@ -1,10 +1,16 @@
 import Contact from '../models/Contact.js'
 import jwt from 'jsonwebtoken'
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const jwtSecret = process.env.JWT_SECRET
 
 // business logic function for adding contact
 export async function createContact(token, contactId, nickname) {
   // using the jwt.verify() function to verify the authenticity and integrity JWT using my secret key.
-  const decoded = jwt.verify(token, JWT_SECRET)
+  const decoded = jwt.verify(token, jwtSecret)
 
   // Extract the user ID from the decoded JWT payload
   const userId = decoded.id;
