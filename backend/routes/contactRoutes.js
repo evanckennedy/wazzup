09bc60a398aaad6a88403d2e86557e92e00f3015
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-21 15:25:33 -0500

    define route for creating contact

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express'
-// import contact controller functions
+import { handleCreateContact } from '../controllers/contactController.js';
 
 // create router instance
 const router = express.Router();
+
+// define a POST route for creating a new contact, handled by the handleCreateContact function
+router.post('/contacts', handleCreateContact)
 
 export default router;
