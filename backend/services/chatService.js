515df7612f462b0e7a9a7be3b5041ebfe7762e14
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-23 17:42:04 -0500

    create service function for creating chat

--- backend/services/chatService.js
+++ backend/services/chatService.js
@@ -1,9 +1,28 @@
+import Chat from '../models/Chat.js'
+import User from '../models/User.js';
+
 export async function createChat(chatData) {
   // get the participants from the chatData
+  const { participants } = chatData
 
   //create new chat document
+  const newChat = new Chat({
+    participants, // shorthand syntax, equivalent to participants: participants
+    messages: [],
+    lastMessage: null
+  })
 
   // save chat to the database
+  await newChat.save();
 
   // add the chat to each participant's chats field
+  // Update multiple User documents in the database
+  await User.updateMany(
+    // Find users whose _id is in the participants array
+    { _id: { $in: participants } },
+    // Update the chats field by adding the new chat's ID to the array
+    { $push: { chats: newChat._id } }
+  );
+
+  return newChat;
 }
