4b7a3d153dafb2a44005c51628389f94f6bfabf2
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-27 16:03:55 -0500

    add register user post request function

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,15 @@ import axios from 'axios'
 const API_URL = 'http://localhost:5000/api';
 
 // User API
+export const registerUser = async userData => {
+  try {
+    const response = await axios.post(`${API_URL}/users`, userData) 
+    return response.data
+  } catch (error) {
+    console.error('Error registering user:', error)
+    throw error;
+  }
+}
 
 // Contact API
 
