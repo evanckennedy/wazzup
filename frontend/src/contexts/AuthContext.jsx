48aedeff5883440ea378d63cbcccd4c6496490cf
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-27 23:13:56 -0500

    create AuthContext.jsx

---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useState } from 'react'
+
+// Create a context for authentication
+const AuthContext = createContext()
+
+// Custom hook to easily access the AuthContext in any component
+export const useAuth = () => useContext(AuthContext);
+
+// AuthProvider component to wrap the entire app
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem('token'));
+
+  const saveToken = newToken => {
+    localStorage.setItem('token', newToken)
+    setToken(newToken)
+  }
+
+  const clearToken = () => {
+    localStorage.removeItem('token');
+    setToken('');
+  }
+
+  return (
+    // <AuthContext.Provider> holds the authentication data
+    <AuthContext.Provider value={{ token, saveToken, clearToken }}>
+      {/* the children prop is being rendered inside the context provider.
+          this means that any components wrapped by the AuthProvider will have access to the authentication context
+      */}
+      {children} 
+    </AuthContext.Provider>
+  )
+}
