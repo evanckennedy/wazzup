37b74b9b717fbfcf1c7931f1f076c94a2f2edca1
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-31 13:42:30 -0500

    display a user's contact by their ID

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ContactItem from './ContactItem'
 
 function ContactList({contacts}) {
+  console.log('Contacts:::', contacts);
   return (
     <ul>
       {contacts.map(contact => (
-        <ContactItem key={contact.contactId} contact={contact}/>
+        <ContactItem key={contact._id} contact={contact}/>
       ))}
     </ul>
   )
--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ChatSidebar from '../components/ChatSidebar'
 import ChatWindow from '../components/ChatWindow'
 import chats from '../mock-data/chats'
-import contacts from '../mock-data/contacts'
+/* import contacts from '../mock-data/contacts' */
 import { useAuth } from '../contexts/AuthContext'
 
 function Chat() {
+  const { contacts } = useAuth()
   /* const { chats, contacts } = useAuth() */
 
   return (
