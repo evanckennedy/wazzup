87686a5a83514eb13fc58e339f06f9f5dab130f4
Evan Kennedy <evanckennedy@protonmail.com>
2024-08-31 12:12:15 -0500

    display user name and id when they log in

--- frontend/src/components/ChatSidebar.jsx
+++ frontend/src/components/ChatSidebar.jsx
@@ -3,24 +3,37 @@ import ChatList from './ChatList'
 import ChatSidebarHeader from './ChatSidebarHeader'
 import ContactList from './ContactList';
 import LogoutButton from './LogoutButton';
+import { useAuth } from '../contexts/AuthContext';
 
 function ChatSidebar({chats, contacts}) {
   // State to manage the selected tab, either 'chats' or 'contacts'
   const [selectedTab, setSelectedTab] = useState('chats');
+
+  // get user info from AuthContext
+  const { user } = useAuth()
 
   // Function to handle tab click events and update the selected tab
   const handleTabClick = (tab) => {
     setSelectedTab(tab);
   }
 
+  const getFirstName = user => {
+    const firstName = user.name.split(' ')[0]
+    return firstName.charAt(0).toUpperCase() + firstName.slice(1);
+  }
+
+  if (!user) {
+    return <div>Loading...</div>; // or any loading indicator
+  }
+
   return (
     <div className='chat-sidebar'>
       <div className='sidebar-user-info'>
         <div className="flex justify-between">
-          <h2 className='welcome-message'>Hi, Evan!</h2>
+          <h2 className='welcome-message'>Hi, {getFirstName(user)}</h2>
           <LogoutButton />
         </div>
-        <p>Your ID: 66ccac9294a87ffba66760f1</p>
+        <p>Your ID: {user._id}</p>
       </div>
       <ChatSidebarHeader selectedTab={selectedTab} handleTabClick={handleTabClick}/>
       {selectedTab === 'chats' ? (
