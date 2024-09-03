b105570b95bbe83c4ec034c69623b4d4c41ba671
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-03 11:22:52 -0500

    create contact functionality

--- frontend/src/components/CreateContactModal.jsx
+++ frontend/src/components/CreateContactModal.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useAuth } from '../contexts/AuthContext'
 
 function CreateContactModal({isOpen, onClose}) {
+  const { createContact } = useAuth()
+  const [contactId, setContactId] = useState('')
+
   if (!isOpen) return null
 
   const handleSubmit = event => {
     event.preventDefault()
 
-    // submission logic
-
-    onClose();
+    try {
+      createContact({contactId})
+      onClose();
+      setContactId('')
+    } catch (error) {
+      console.error('Error creating contact:', error)
+    } 
   }
 
   return (
@@ -17,7 +25,13 @@ function CreateContactModal({isOpen, onClose}) {
         <h2>Create Contact</h2>
         <form onSubmit={handleSubmit}>
           <label htmlFor="contactId">Contact ID:</label>
-          <input type="text" id='contactId' placeholder="Enter your contact's id" />
+          <input 
+            type="text" 
+            id='contactId' 
+            placeholder="Enter your contact's id" 
+            value={contactId}
+            onChange={e => setContactId(e.target.value)}
+            />
           <button type='submit'>Create</button>
           <button type='button' onClick={onClose}>Cancel</button>
         </form>
