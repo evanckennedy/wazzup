1f2ae56cd267ad2bcd67710823846efe1d2bf5f6
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-03 15:01:17 -0500

    implement checkbox button functionality for adding chat members

--- frontend/src/components/CreateChatModal.jsx
+++ frontend/src/components/CreateChatModal.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
-import { getUserContacts } from '../services/api';
+import React, { useEffect, useState } from 'react'
+import { useAuth } from '../contexts/AuthContext'
 
 function CreateChatModal({ isOpen, onClose}) {
+  const { contacts, createChat } = useAuth()
+  const [selectedContacts, setSelectedContacts] = useState([])
+
+  // testing
+  useEffect(() => {
+    console.log("Selected Contacts:", selectedContacts);
+  }, [selectedContacts]);
+
   if (!isOpen) return null; 
 
   const handleSubmit = event => {
     event.preventDefault();
     
-    // submission logic
+    //testing - remove later
+    console.log({participants: selectedContacts})
 
     onClose();
+  }
+
+  const handleContactChange = event => {
+    const contactId = event.target.value
+
+    // If the box is checked, add the contact Id to the selected contacts. Otherwise, remove it from the selected contacts
+    setSelectedContacts(prevSelectedContacts => 
+      event.target.checked 
+        ? [...prevSelectedContacts, contactId]
+        : prevSelectedContacts.filter(id => id !== contactId)
+    )
   }
 
   return (
@@ -17,7 +37,16 @@ function CreateChatModal({ isOpen, onClose}) {
       <div>
         <h2>Create Chat</h2>
         <form onSubmit={handleSubmit}>
-          
+          {contacts.map(contact => (
+            <div key={contact._id}>
+              <input 
+                type="checkbox"
+                value={contact.contactId._id}
+                onChange={handleContactChange} 
+              />
+              {contact.contactId.username}
+            </div>
+          ))}
           <button type='submit'>Create</button>
           <button type='button' onClick={onClose}>Cancel</button>
         </form>
