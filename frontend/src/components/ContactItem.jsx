3b2a4698322dbc703556f4c30e5670565e438035
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-08 18:53:10 -0500

    execute delete contact functionality when trash icon is clicked

--- frontend/src/components/ContactItem.jsx
+++ frontend/src/components/ContactItem.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { FiTrash } from 'react-icons/fi';
+import { useAuth } from '../contexts/AuthContext';
 
 function ContactItem({contact}) {
-  return (
+  const { deleteContact } = useAuth();
+  
+  const handleDelete = async () => {
+    try {
+      await deleteContact(contact._id)
+    } catch (error) {
+      console.error('Error deleting contact:', error)
+    }
+  }
 
+  return (
     <li className='flex justify-between item-info-container'>
       <p className='contact-username'>{contact.contactId.username}</p>
-      <button className='delete-contact-btn'>
+      <button className='delete-contact-btn' onClick={handleDelete}>
         <FiTrash />
       </button>
     </li>
