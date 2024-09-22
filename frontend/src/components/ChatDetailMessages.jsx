f57f7088f3c86c3b70c0970b875e57a0006ce8a0
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-22 17:02:42 -0500

    remove comment

--- frontend/src/components/ChatDetailMessages.jsx
+++ frontend/src/components/ChatDetailMessages.jsx
@@ -27,15 +27,6 @@ function ChatDetailMessages({chat}) {
     })
   }
 
-  /* 
-    To Do: 
-      - When the user clicks on a chat from the sidebar, the messages should display.
-        Map out each chat.
-      - This component should take up the vast majority (roughly 80 percent) of the 
-        vertical spacing of the chat window.
-      - Add "overflow-y: auto;" to the parent div of this component, allowing
-        users to scroll through the message history
-  */ 
   return (
     <div className='messages-container flex flex-column gap-10'>
       {chatMessages.map((message) => (
