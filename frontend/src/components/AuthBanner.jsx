3e421f17d0ae44db84cd4436afbaddfd2590d5ff
Evan Kennedy <evanckennedy@protonmail.com>
2024-10-20 10:27:18 -0500

    organize code

--- frontend/src/components/AuthBanner.jsx
+++ frontend/src/components/AuthBanner.jsx
@@ -5,9 +5,9 @@ import WelcomeMessage from './WelcomeMessage'
 function AuthBanner({isLoginDisplay}) {
   return (
     <section className='flex flex-column auth-section auth-banner'>
-        <img className='auth-banner-img' src={AuthBannerImg} alt="authorization banner image" />
-        <WelcomeMessage isLoginDisplay={isLoginDisplay}/>
-      </section>
+      <img className='auth-banner-img' src={AuthBannerImg} alt="authorization banner image" />
+      <WelcomeMessage isLoginDisplay={isLoginDisplay}/>
+    </section>
   )
 }
 
